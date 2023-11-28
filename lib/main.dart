d1ad9a3cef36235181f3c0a716e38014bc294ab5
sanketsakpal <sakpalsanket966@gmail.com>
2023-11-28 18:01:10 +0530

    - remove unused imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 
 import 'package:animate_flutter/explicit_animation_example/animated_prompt.dart';
-import 'package:animate_flutter/explicit_animation_example/drawer.dart';
 import 'package:flutter/material.dart';
 
 void main() {
